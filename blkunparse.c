f552cce9a1e698a9c1f1b523f458d44a4c028686
omkarbdesai <omkarbdesai@gmail.com>
2021-06-24 17:24:31 -0400

    first commit

---
+++ blkunparse.c
@@ -0,0 +1,143 @@
+//
+// Created by Omkar Desai on 6/24/21.
+//
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <errno.h>
+#include <signal.h>
+#include <locale.h>
+#include <libgen.h>
+
+#include "blktrace.h"
+#include "rbtree.h"
+#include "jhash.h"
+
+static char blkunparse_version[] = "0.1";
+
+static FILE *dump_fp;
+static char *dump_binary;
+
+static struct option l_opts[] = {
+        {
+                .name = "input",
+                .has_arg = required_argument,
+                .flag = NULL,
+                .val = 'i'
+        },
+        {
+                .name = "version",
+                .has_arg = no_argument,
+                .flag = NULL,
+                .val = 'V'
+        },
+        {
+                .name = NULL,
+        }
+};
+
+static int name_fixup(char *name)
+{
+    char *b;
+
+    if (!name)
+        return 1;
+
+    b = strstr(name, ".blkparse.");
+    if (b)
+        *b = '\0';
+
+    return 0;
+}
+
+static int do_btrace_file(void){
+    // name_fixup();
+    //if (ret)
+    //    return ret;
+    
+
+}
+
+static char usage_str[] =    "\n\n" \
+	"-i <file>           | --input=<file>\n" \
+	"[ -V                | --version ]\n\n" \
+	"\t-i Input file containing trace data, or '-' for stdin\n" \
+	"\t-V Print program version info\n\n";
+
+static void usage(char *prog){
+
+    fprintf(stderr, "Usage: %s %s", prog, usage_str);
+}
+
+
+int main(int argc, char *argv[]){
+    int c, ret, mode;
+    char *ofp_buffer = NULL;
+    char *bin_ofp_buffer = NULL;
+
+    while ((c = getopt_long(argc, argv, S_OPTS, l_opts, NULL)) != -1) {
+        switch (c) {
+            case 'i':
+                if (is_pipe(optarg) && !pipeline) {
+                    pipeline = 1;
+                    pipename = strdup(optarg);
+                } else if (resize_devices(optarg) != 0)
+                    return 1;
+                break;
+
+            case 'd':
+                dump_binary = optarg;
+                break;
+
+            case 'V':
+                printf("%s version %s\n", argv[0], blkparse_version);
+                return 0;
+
+            default:
+                usage(argv[0]);
+                return 1;
+        }
+    }
+
+    signal(SIGINT, handle_sigint);
+    signal(SIGHUP, handle_sigint);
+    signal(SIGTERM, handle_sigint);
+
+    setlocale(LC_NUMERIC, "en_US");
+
+
+    if (dump_binary) {
+        if (!strcmp(dump_binary, "-"))
+            dump_fp = stdout;
+        else {
+            dump_fp = fopen(dump_binary, "w");
+            if (!dump_fp) {
+                perror(dump_binary);
+                dump_binary = NULL;
+                return 1;
+            }
+        }
+        bin_ofp_buffer = malloc(128 * 1024);
+        if (setvbuf(dump_fp, bin_ofp_buffer, _IOFBF, 128 * 1024)) {
+            perror("setvbuf binary");
+            return 1;
+        }
+    }
+
+    ret = do_btrace_file();
+
+    if (ofp_buffer) {
+        fflush(ofp);
+        free(ofp_buffer);
+    }
+    if (bin_ofp_buffer) {
+        fflush(dump_fp);
+        free(bin_ofp_buffer);
+    }
+    return ret;
+}
